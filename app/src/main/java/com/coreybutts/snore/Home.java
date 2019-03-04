43fef72dbcf0f1537442c8962471e0ad183c369e
Borey-PC <buttscl@mail.uc.edu>
2019-03-04 11:36:09 -0500

    Added a home screen

--- app/src/main/java/com/coreybutts/snore/Home.java
+++ app/src/main/java/com/coreybutts/snore/Home.java
@@ -1,18 +1,32 @@
 package com.coreybutts.snore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+    Button buttonSleep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        buttonSleep = (Button) findViewById(R.id.button_sleep);
+
+        buttonSleep.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sleepIntent = new Intent(Home.this, Sleep.class);
+                startActivity(sleepIntent);
+                finish();
+            }
+        });
     }
 
 }
